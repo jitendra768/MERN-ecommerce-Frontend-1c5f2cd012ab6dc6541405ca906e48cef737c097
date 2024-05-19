b93b076d130f6cf1ec8b936859ca6e8c89b7f5bd
jackjitendra768 <jackjitendra768@gmail.com>
2024-05-19 16:30:32 +0530

    e-commerce-website

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../features/auth/Components/Login'
+
+const LoginPage = () => {
+  return (
+    <div>
+<Login/>
+    </div>
+  )
+}
+
+export default LoginPage
