6a2c73852f9101c4ba1db29b3e83de21e4d91d1c
jackjitendra768 <jackjitendra768@gmail.com>
2024-05-22 17:36:31 +0530

    shoping-cart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import CartPage from './pages/CartPage';
 
 
 const router = createBrowserRouter([
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage/>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage/>,
   },
 ]);
 
